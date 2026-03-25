b8fafacc61270ffa44b738a449ab25dc3bc09d67
Leymz <ridwanadekunle16@email.com>
2026-03-25 10:20:08 +0100

    fix suspense

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-      os: false,
-      crypto: false,
-    };
-    return config;
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
 };
 
